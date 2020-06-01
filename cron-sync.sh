27feb515d9cb7ce917425a94d3b80028a09cf9da
fankainang <fankainang@gmail.com>
2020-06-01 12:08:15 +0800

    优化打日志

--- cron-sync.sh
+++ cron-sync.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 set -Ce
 
-# looking for the dir to be sync
-if [ ! $AUTO_SYNC_DIR ];then
-  echo "Please set up \$AUTO_SYNC_DIR first"
-  exit 1
-fi
-echo "sync dir: " $AUTO_SYNC_DIR
-
-# setup logging dir
-
-SCRIPT_DIR=$(dirname $(realpath $0))
 LOG_DIR=$SCRIPT_DIR/log
 echo $LOG_DIR
 mkdir -p $LOG_DIR
@@ -19,9 +9,20 @@ if [ ! -f $LOG_FILE ];then
   touch $LOG_FILE
 fi
 
+# looking for the dir to be sync
+if [ ! $AUTO_SYNC_DIR ];then
+  echo "Please set up \$AUTO_SYNC_DIR first" >2& | tee -a $LOG_FILE
+  exit 1
+fi
+echo "sync dir: " $AUTO_SYNC_DIR
+
+# setup logging dir
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
 # sync and log
 pushd $AUTO_SYNC_DIR
 echo "note dir: " $AUTO_SYNC_DIR | tee -a $LOG_FILE
-echo "===================="`date +%Y-%m-%dT%H:%M:%S`"====================" | tee -a $LOG_FILE
+echo "===================="`date +%Y-%m-%dT%H:%M:%S`"====================" >2& | tee -a $LOG_FILE
 $SCRIPT_DIR/git-sync sync | tee -a $LOG_FILE 
 popd
