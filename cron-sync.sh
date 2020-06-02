972e74aaddd9cab9776bd4f0fd62f632c622a280
fankainang <fankainang@gmail.com>
2020-06-02 11:52:17 +0800

    调整打日志位置

--- cron-sync.sh
+++ cron-sync.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -Ce
 
+# setup logging dir
+SCRIPT_DIR=$(dirname $(realpath $0))
 LOG_DIR=$SCRIPT_DIR/log
 echo $LOG_DIR
 mkdir -p $LOG_DIR
@@ -11,18 +13,14 @@ fi
 
 # looking for the dir to be sync
 if [ ! $AUTO_SYNC_DIR ];then
-  echo "Please set up \$AUTO_SYNC_DIR first" >2& | tee -a $LOG_FILE
+  echo "Please set up \$AUTO_SYNC_DIR first" | tee -a $LOG_FILE
   exit 1
 fi
 echo "sync dir: " $AUTO_SYNC_DIR
 
-# setup logging dir
-
-SCRIPT_DIR=$(dirname $(realpath $0))
-
 # sync and log
 pushd $AUTO_SYNC_DIR
 echo "note dir: " $AUTO_SYNC_DIR | tee -a $LOG_FILE
-echo "===================="`date +%Y-%m-%dT%H:%M:%S`"====================" >2& | tee -a $LOG_FILE
+echo "===================="`date +%Y-%m-%dT%H:%M:%S`"====================" | tee -a $LOG_FILE
 $SCRIPT_DIR/git-sync sync | tee -a $LOG_FILE 
 popd
